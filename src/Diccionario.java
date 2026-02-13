39bbb3185050185dbf510849ee4c8f651f684341
Dafne Hernandez <mcflurrycerezita@gmail.com>
2026-02-13 17:29:33 -0600

    feat: initial file dictionary

---
+++ src/Diccionario.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+
+public class Diccionario {
+    public static void main(String[]args){
+        HashMap<String, String> diccionario = new HashMap<>();
+
+        // Adding elements
+        diccionario.put("Apple", "Manzana");
+        diccionario.put("Orange", "Naranja");
+        diccionario.put("Strawberry", "Fresa");
+
+        diccionario.put("Love", "Amor");
+        diccionario.put("Friendship", "Amistad");
+        diccionario.put("Mix", "Mezclar");
+
+        diccionario.put("Travel", "Viajar");
+        diccionario.put("Drink", "Beber");
+        diccionario.put("Eat", "Comer");
+
+        diccionario.put("Spread", "Untar");
+        diccionario.put("Sprinkle", "Espolvorear");
+        diccionario.put("Cut", "Cortar");
+
+        diccionario.put("Damage", "Dañar");
+        diccionario.put("Hit", "Golpear");
+        diccionario.put("Heal", "Sanar");
+
+        diccionario.put("Mystery", "Misterio");
+        diccionario.put("Case", "Caso");
+        diccionario.put("Clue", "Pista");
+
+        diccionario.put("Job", "Trabajo");
+        diccionario.put("Salary", "Salario");
+
+        String traduccion;
+
+    }
+
+}
